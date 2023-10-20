f6995535292466ad5058b53369bcc3824dff1844
karen0405 <akab09029@gmail.com>
2023-10-20 17:49:12 -0600

    page corregida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-
 import Nav from '@/app/Components/Nav';
 import Body from "@/app/Components/Body";
-export default function home () {
+export default function main () {
   return(
-      <main className="flex">
-import Nav from "@//Components/Head";
-import Body from "@/Components/Share";
-export default function home(){
-  return(
-      <main className="">
+        <div>
         <Nav/>
         <Body/>
-      </main>
+       </div>
   )
 }
